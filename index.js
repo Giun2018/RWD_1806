2c6bef9bc81532a74b1ca2e4cb04721c5787159e
巨匠電腦超級駭客 <39856299+Giun2018@users.noreply.github.com>
2018-06-12 21:47:47 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 $(".menu").click(function(){
-	$(".navouter").fadeIn();
+	$(".navouter").fadeIn(1000);
 });
+
+$(".fa-times .navouter").click(function(){
+	$(".navouter").fadeOut();
+						  });
+$(".othereles").click(function(){$(".n1").css("right","0")})
