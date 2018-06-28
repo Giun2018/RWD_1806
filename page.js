037acf151c8ecacb53b94c5a3d2c9f0c51f61692
巨匠電腦超級駭客 <39856299+Giun2018@users.noreply.github.com>
2018-06-28 21:49:34 +0800

    更新BOT.HTM
    更新一堆雜七雜八的東西。
    
    https://github.com/mafintosh/torrent-stream

--- page.js
+++ page.js
@@ -1,3 +1,24 @@
 $('.carousel').carousel({
   interval: 2000
 });
+$(".cdsc>img").click(function{
+	var .".cdsc"=$(data).('cds');
+	$(".cdn").fadeIn.();
+	$(".cdo").children.hide.();
+				  });
+$("href").click(function{
+	var ."href"=$(data).('cds');
+	$(".cdn").fadeIn.();
+	$(".cdo").children.hide.();
+				  });
+$(".cdm").click(function(){
+	$(".cdn").fadeOut();
+});
+$(Document).keydown(function(){
+	$(".cdn").fadeOut();
+});
+$(Document).keydown(function(e){
+	if(e.keyCode===27){
+	   $(".cdn").fadeOut();
+	   }
+});
