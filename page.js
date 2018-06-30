a46bee45f83c4ff9b1057e0e562347cd018fb336
巨匠電腦超級駭客 <39856299+Giun2018@users.noreply.github.com>
2018-06-30 21:37:56 +0800

    不確定的更新
    我也不知道要更新什麼？

--- page.js
+++ page.js
@@ -1,24 +1,47 @@
 $('.carousel').carousel({
   interval: 2000
 });
-$(".cdsc>img").click(function{
-	var .".cdsc"=$(data).('cds');
-	$(".cdn").fadeIn.();
-	$(".cdo").children.hide.();
-				  });
-$("href").click(function{
-	var ."href"=$(data).('cds');
-	$(".cdn").fadeIn.();
-	$(".cdo").children.hide.();
-				  });
-$(".cdm").click(function(){
-	$(".cdn").fadeOut();
-});
-$(Document).keydown(function(){
-	$(".cdn").fadeOut();
-});
-$(Document).keydown(function(e){
-	if(e.keyCode===27){
-	   $(".cdn").fadeOut();
-	   }
+//$(".cdsc>img").click(function{
+//	var .".cdsc"=$(data).('cds');
+//	$(".cdn").fadeIn.();
+//	$(".cdo").children.hide.();
+//				  });
+//$("href").click(function{
+//	var ."href"=$(data).('cds');
+//	$(".cdn").fadeIn.();
+//	$(".cdo").children.hide.();
+//				  });
+//$(".cdm").click(function(){
+//	$(".cdn").fadeOut();
+//});
+//$(Document).keydown(function(){
+//	$(".cdn").fadeOut();
+//});
+//$(Document).keydown(function(e){
+//	if(e.keyCode===27){
+//	   $(".cdn").fadeOut();
+//	   }
+//});
+
+//$(document).ready(function(){
+//
+//    //Check to see if the window is top if not then display button
+//    $(window).scroll(function(){
+//        if ($(this).scrollTop() > 100) {
+//            $('.scrollToTop').fadeIn();
+//        } else {
+//            $('.scrollToTop').fadeOut();
+//        }
+//    });
+//
+//    //Click event to scroll to top
+//    $('.scrollToTop').click(function(){
+//        $('html, body').animate({scrollTop : 0},800);
+//        return false;
+//    });
+//
+//});
+
+$('.ST').click(function(){
+	$('html, body').animate({screenTop:0});
 });
