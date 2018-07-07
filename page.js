e16207d766c44d6c692be56d12af864bc3628e54
巨匠電腦超級駭客 <39856299+Giun2018@users.noreply.github.com>
2018-07-07 21:49:37 +0800

    檔案遺失重新更新

--- page.js
+++ page.js
@@ -49,10 +49,40 @@ $(document).ready(function(){
 //	if ($('.totop').fadeIn((this).()>100))
 //	else ($('totop').fadeOut(this.()<100));
 //});
-$(function(){ //文字提示
-  $(".tooltip").tooltip(options)
-  });
-$(".nav-link").click(function(){
-	var navlink=$(this).attr("href");
-	$("html").stop().animat({scrollTop:$(navlink).offset().top-150});
-});
+//$(function(){ //文字提示
+//  $(".tooltip").tooltip(options)
+//  });
+//$(".nav-link").click(function(){
+//	var navlink=$(this).attr("href");
+//	$("html").stop().animat({scrollTop:$(navlink).offset().top-150});
+//});
+var options = {
+   animationDuration: 0.5, // in seconds
+   filter: 'all', // Initial filter
+   callbacks: { 
+      onFilteringStart: function() { },
+      onFilteringEnd: function() { },
+      onShufflingStart: function() { },
+      onShufflingEnd: function() { },
+      onSortingStart: function() { },
+      onSortingEnd: function() { }
+   },
+   controlsSelector: '', // Selector for custom controls
+   delay: 100, // Transition delay in ms
+   delayMode: 'alternate', // 'progressive' or 'alternate'
+   easing: 'ease-out',
+   filterOutCss: { // Filtering out animation
+      opacity: 0,
+      transform: 'scale(0.5)'
+   },
+   filterInCss: { // Filtering in animation
+      opacity: 0,
+      transform: 'scale(1)'
+   },
+   layout: 'sameSize', // See layouts
+   multifilterLogicalOperator: 'or',
+   selector: '.filtr-container',
+   setupControls: true // Should be false if controlsSelector is set 
+} 
+// You can override any of these options and then call...
+var filterizd = $('.filtr-container').filterizr(options);
